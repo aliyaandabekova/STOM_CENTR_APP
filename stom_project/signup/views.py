dc5f8b0f7337ffe4367737f3745e7faa3fd49c9e
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-30 18:13:46 +0300

    create classes: DayViewSet, DoctorViewSet

---
+++ stom_project/signup/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from .serializers import *
+from .permissions import IsSuperUser,DoctorDayPermission
+
+class DayViewSet(ViewSet):
+    permission_classes = [IsSuperUser]
+    serializer_class = DaySerializer
+
+    def list(self,request):
+        days = Day.objects.all()
+        serializer = self.serializer_class(days,many=True)
+        return Response(serializer.data,status=200)
+    def create(self,request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=201)
+    def retrieve(self,request,day_id):
+        day = Day.objects.get(id=day_id)
+        serializer = self.serializer_class(day)
+        return Response(serializer.data,status=200)
+    def update(self,request,day_id):
+        day = Day.objects.get(id=day_id)
+        serializer = self.serializer_class(day,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=202)
+    def destroy(self,request,day_id):
+        day = Day.objects.get(id=day_id)
+        day.delete()
+        return Response(status=204)
+
+
+class DoctorDayView(ViewSet):
+    serializer_class = DoctorDaySerializer
+    permission_classes = [DoctorDayPermission]
+
+    def list(self,request):
+        doctor_days = DoctorDay.objects.filter(doctor=request.user)
+        serializer = DoctorDaySerializer(doctor_days,many=True)
+        return Response(serializer.data)
+
+    def create(self,request,day_id):
+        day = Day.objects.get(id=day_id)
+        work_day = DoctorDay.objects.create(doctor=request.user,day=day)
+        serializer = self.serializer_class(work_day)
+        return Response(serializer.data)
+
+    def update(self,request,doctor_day_id):
+        work_day = DoctorDay.objects.get(id=doctor_day_id)
+        serializer = DoctorDaySerializer(work_day,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+    def retrieve(self, request, doctor_day_id):
+        work_day = DoctorDay.objects.get(id=doctor_day_id)
+        serializer = DoctorDaySerializer(work_day)
+        return Response(serializer.data)
