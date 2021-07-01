d276f453ff6d9204650d69a60df60a0155290586
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-07-01 18:21:03 +0300

    register Order-model

--- stom_project/signup/admin.py
+++ stom_project/signup/admin.py
@@ -7,3 +7,8 @@ class DayDoctorDisplay(admin.ModelAdmin):
     list_display = ['doctor','day']
 admin.site.register(DoctorDay,DayDoctorDisplay)
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['day','doctor','date_created','client']
+admin.site.register(Order,OrderAdmin)
+
+
