54a9c02ec28803eeec2a6ddf9e7c61813c0dd0d4
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-07-01 18:21:58 +0300

    create OrderPermissions

--- stom_project/signup/permissions.py
+++ stom_project/signup/permissions.py
@@ -26,3 +26,19 @@ class DoctorDayPermission(permissions.BasePermission):
             elif group == 'manager' and request.method == "DELETE":
                 return True
 
+class OrderPermissions(permissions.BasePermission):
+    def has_permission(self, request, view):
+
+        group = None
+        if isinstance(request.user, User):
+            group = request.user.groups.all()[0].name
+
+        if request.method in SAFE_METHODS:
+            return True
+        if group == 'client' and request.method == 'POST':
+            return True
+        if group == 'manager' and request.method in ['GET','PUT','DELETE']:
+            return True
+
+
+
