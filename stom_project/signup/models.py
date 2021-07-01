72ee8602815854403ec4a7d2254c36c307c00fb6
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-07-01 18:21:34 +0300

    create Order-model, add parameter 'status' in Doctorday

--- stom_project/signup/models.py
+++ stom_project/signup/models.py
@@ -11,3 +11,13 @@ class Day(models.Model):
 class DoctorDay(models.Model):
     doctor = models.ForeignKey(User, on_delete=models.CASCADE)
     day = models.ForeignKey(Day, on_delete=models.CASCADE)
+    status = models.CharField(choices=(
+        ('free','free'),
+        ('reserved','reserved')
+    ),max_length=10,default='free')
+
+class Order(models.Model):
+    client = models.ForeignKey(User,on_delete=models.CASCADE,related_name='clients')
+    doctor = models.ForeignKey(User,on_delete=models.CASCADE,related_name='doctors')
+    day = models.ForeignKey(Day,on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
