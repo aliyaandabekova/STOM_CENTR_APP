f6dfefe28758ce2ee0a46ba6df8689afd92f9fdf
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-07-06 17:29:43 +0300

    add unique in name of Day

--- stom_project/signup/models.py
+++ stom_project/signup/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Day(models.Model):
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=10,unique=True)
 
     def __str__(self):
         return self.name
